1c91a6b3aa65ac3b11ee36af1ff6b0de5b43b0fc
Juan Carlos Cardenas Viera <jcc@Juans-MacBook-Pro.local>
2017-10-17 19:45:09 -0500

    unit tests COMPLETE

--- lesson5_django/my_first_project/personas/tests.py
+++ lesson5_django/my_first_project/personas/tests.py
@@ -16,13 +16,28 @@ class PersonasTest(TestCase):
         self.primer_persona = Personas.objects.create(
             nombre="Juan Carlos",
             edad=35,
+            sexo="M",
             tipo_de_personas="Otro"
             )
-        self.primer_persona = Personas.objects.create(
+        self.segunda_persona = Personas.objects.create(
             nombre="Safot",
             edad=50,
+            sexo="I",
             tipo_de_personas="Voluntario"
             )
+        self.persona_incorrecta_json = {
+            "nombre":"Eduardod",
+            "edad":14,
+            "sexo":5,
+            "tipo_de_personas":"Otro"
+        }
+        self.persona_correcta_json = {
+            "nombre" :"Eduardod",
+            "edad":"56",
+            "sexo":"M",
+            "tipo_de_personas":"Otro"
+        }
+
         
 
     def test_get_all_personas(self):
@@ -31,6 +46,17 @@ class PersonasTest(TestCase):
         serializer = PersonasSerializer(personas, many=True)
         self.assertEquals(response.status_code,200)
         self.assertEquals(serializer.data,response.data)
-    
 
+    def test_get_persona(self):
+        response = self.client.get(reverse("persona_end_point", kwargs={'pk':self.primer_persona.id}))
+        persona = Personas.objects.get(pk=self.primer_persona.id)
+        serializer = PersonasSerializer(persona)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(serializer.data, response.data)
+    
+    def test_post_persona(self):
+        response = self.client.post(reverse('personas_end_point'), 
+            data=json.dumps(self.persona_correcta_json),
+            content_type='application/json')
+        self.assertEquals(response.status_code, 201)
 # Create your tests here.
